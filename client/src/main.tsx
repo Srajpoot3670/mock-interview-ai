6b59c26987147ea4d854d95f5a93a98cc4d4fe3f
Srajpoot3670 <85182601+Srajpoot3670@users.noreply.github.com>
2026-02-17 17:05:29 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Agar index.css hai toh, nahi toh ye line hata dein
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
